37bfa4d071e7620541e696cd00620ec0d2c873ba
josolha <josolha@nate.com>
2023-03-10 14:02:26 +0900

    feat : 상품 DB 테이블, DTO 생성 완료.

---
+++ shoppingMall/src/main/java/com/web/jomaltwo/model/ProductDTO.java
@@ -0,0 +1,24 @@
+package com.web.jomaltwo.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+		private int pNum;
+		private String pName;
+		private String pCategory_fk;
+		private String pCompany;
+		private String pImage;
+		private int pQty;
+		private int price;
+		private String pSpec;
+		private String pContent;
+		private int pPoint;
+		private Date pInputDate;
+		
+		
+		private int totPrice;
+		private int totPoint;
+}
